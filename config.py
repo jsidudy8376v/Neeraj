47824838b1e9d726ee663f866a32fb7aee3fa1e9
jsidudy8376v <rahul88424n@gmail.com>
2025-03-03 21:39:23 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+BOT_TOKEN = "7760385398:AAHEXpx1Igmd1mohGyVYBlg0KEWs2WfNYTg"
+API_ID = 25873863 # अपना Telegram API ID
+API_HASH = "f1508ba00c6a5ae302a95a29e0a2c43d"
+OWNER_ID = [6495160867]  # Owner Telegram ID डाल
+BANNED_WORDS = ["group Uda dunga ", "Group Gaya ", "abusiveword"]  # अपने हिसाब से Words
+DEFAULT_ROLE = "Member"
+AI_API_KEY = "(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)"
